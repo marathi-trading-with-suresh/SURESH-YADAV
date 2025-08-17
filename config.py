d8faf5bc5d74df784048072e5d70998cc82f109f
marathi-trading-with-suresh <sy23625@gmail.com>
2025-08-17 22:03:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+CSV_PATH = "C:/Users/ASUS/Downloads/Telegram Desktop/Nifty200list.csv"
+
+EMOJI_VERDICTS = {
+    "Bullish": "📈",
+    "Bearish": "📉",
+    "Neutral": "⚖️"
+}
+
+HASHTAGS = "#Intraday #MarathiTrading #StockMarketIndia"
